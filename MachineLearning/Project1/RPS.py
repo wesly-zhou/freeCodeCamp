081ea78e5c774418ce7b0c2ce5ce4b7ebf08b58a
Wesly Zhou <wesly.zhou@tufts.edu>
2025-01-13 15:33:04 -0500

    Upload of Rock Paper Scissors starter code

---
+++ MachineLearning/Project1/RPS.py
@@ -0,0 +1,10 @@
+# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago.
+
+def player(prev_play, opponent_history=[]):
+    opponent_history.append(prev_play)
+
+    guess = "R"
+    if len(opponent_history) > 2:
+        guess = opponent_history[-2]
+
+    return guess
